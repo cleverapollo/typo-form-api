b245a0a1b2606daab5c5c193dfc2d69fe9e8354c
cleverapollo <cleverapollo@gmail.com>
2018-02-23 16:36:42 +0000

    Solved CORS with option methods

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Response;
 
 class CorsMiddleware
 {
@@ -15,7 +16,11 @@ class CorsMiddleware
      */
     public function handle($request, Closure $next)
     {
-        $response = $next($request);
+        if ($request->isMethod('OPTIONS')) {
+            $response = new Response('', 200);
+        } else {
+            $response = $next($request);
+        }
         $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, PATCH, DELETE');
         $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
         $response->header('Access-Control-Allow-Origin', '*');
