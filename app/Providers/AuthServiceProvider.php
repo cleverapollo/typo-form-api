b17f0e3fc2be2014edc6fbcd3cf7089f73ea65f8
cleverapollo <cleverapollo@gmail.com>
2018-02-25 22:17:36 +0000

    Changed to Carbon

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -33,7 +33,7 @@ class AuthServiceProvider extends ServiceProvider
 
         $this->app['auth']->viaRequest('api', function ($request) {
             if ($request->header('api_token')) {
-                return User::where('expire_date', '>=', Carbon::yesterday())
+                return User::where('expire_date', '>', Carbon::yesterday())
                     ->where('api_token', $request->header('api_token'))->first();
             }
             // if ($request->input('api_token')) {
