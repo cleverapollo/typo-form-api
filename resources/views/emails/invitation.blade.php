f62f13fade59bcf0384c90722ee1447d7759c285
cleverapollo <cleverapollo@gmail.com>
2018-03-09 20:40:21 +0800

    Fix invitation link

--- resources/views/emails/invitation.blade.php
+++ resources/views/emails/invitation.blade.php
@@ -8,6 +8,6 @@
 <body>
     <h1>Join {{$name}} on Informed 365</h1>
     <p>You are invited to the {{$type}} {{$name}} as a {{$role}} by {{$userName}}.</p>
-    <a href="'http://localhost:8080/invitation/' . {{$token}}">View invitation</a>
+    <a href="http://localhost:8080/invitation/{{$token}}">View invitation</a>
 </body>
 </html>
