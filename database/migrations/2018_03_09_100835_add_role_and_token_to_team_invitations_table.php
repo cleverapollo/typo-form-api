b10f96e9da3d2cc27ec5475aa02bb781fcf303b6
cleverapollo <cleverapollo@gmail.com>
2018-03-09 18:09:46 +0800

    Add role and token to invitation tables

---
+++ database/migrations/2018_03_09_100835_add_role_and_token_to_team_invitations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRoleAndTokenToTeamInvitationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('team_invitations', function (Blueprint $table) {
+            $table->string('role')->default('User')->after('team_id');
+            $table->string('token')->after('role');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('team_invitations', function (Blueprint $table) {
+            $table->dropColumn('token');
+            $table->dropColumn('role');
+        });
+    }
+}
