ba55d5d37f24c8cea1ef59b10ef82aef5687bfc4
cleverapollo <cleverapollo@gmail.com>
2018-03-17 10:23:44 +0100

    Update group controller

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Section;
+use App\Http\Resources\GroupResource;
 use Illuminate\Http\Request;
 
 class GroupController extends Controller
@@ -17,117 +18,131 @@ class GroupController extends Controller
         $this->middleware('auth:api');
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @param  int $section_id
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function index($section_id)
-    {
-        $groups = Section::find($section_id)->groups()->get();
-        return response()->json([
-            'status' => 'success',
-            'groups' => $groups
-        ], 200);
-    }
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @param  int $section_id
+	 * @return \Illuminate\Http\JsonResponse
+	 */
+	public function index($section_id)
+	{
+		$groups = Section::find($section_id)->groups()->get();
+		return $this->returnSuccessMessage('groups', GroupResource::collection($groups));
+	}
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  int $section_id
-     * @param  \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store($section_id, Request $request)
-    {
-        $this->validate($request, [
-            'name' => 'required|max:191'
-        ]);
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @param  int $section_id
+	 * @param  \Illuminate\Http\Request $request
+	 * @return \Illuminate\Http\JsonResponse
+	 */
+	public function store($section_id, Request $request)
+	{
+		$section = Section::find($section_id);
 
-        $group = Section::find($section_id)->groups()->create($request->all());
-        if ($group) {
-            return response()->json([
-                'status' => 'success',
-                'group' => $group
-            ], 200);
-        }
-        return response()->json([
-            'status' => 'fail',
-            'message' => $this->generateErrorMessage('group', 503, 'store')
-        ], 503);
-    }
+		// Send error if section does not exist
+		if (!$section) {
+			return $this->returnErrorMessage('section', 404, 'create group');
+		}
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int $section_id
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($section_id, $id)
-    {
-        $group = Section::find($section_id)->groups()->where('id', $id)->first();
-        if ($group) {
-            return response()->json([
-                'status' => 'success',
-                'group' => $group
-            ], 200);
-        }
-        return response()->json([
-            'status' => 'fail',
-            'message' => $this->generateErrorMessage('group', 404, 'show')
-        ], 404);
-    }
+		// Create group
+		$group = $section->groups()->create($request->only('name', 'repeatable'));
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  int $section_id
-     * @param  int $id
-     * @param  \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
-    public function update($section_id, $id, Request $request)
-    {
-        $this->validate($request, [
-            'name' => 'filled'
-        ]);
+		if ($group) {
+			return $this->returnSuccessMessage('group', new GroupResource($group));
+		}
 
-        $group = Section::find($section_id)->groups()->where('id', $id)->first();
-        if (!$group) {
-            return response()->json([
-                'status' => 'fail',
-                'message' => $this->generateErrorMessage('group', 404, 'update')
-            ], 404);
-        }
-        if ($group->fill($request->all())->save()) {
-            return response()->json([
-                'status' => 'success',
-                'group' => $group
-            ], 200);
-        }
-        return response()->json([
-            'status' => 'fail',
-            'message' => $this->generateErrorMessage('group', 503, 'update')
-        ], 503);
-    }
+		// Send error if group is not created
+		return $this->returnErrorMessage('group', 503, 'create');
+	}
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int $section_id
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($section_id, $id)
-    {
-        if (Section::find($section_id)->groups()->where('id', $id)->delete()) {
-            return response()->json(['status' => 'success'], 200);
-        }
-        return response()->json([
-            'status' => 'fail',
-            'message' => $this->generateErrorMessage('group', 503, 'delete')
-        ], 503);
-    }
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int $section_id
+	 * @param  int $id
+	 * @return \Illuminate\Http\JsonResponse
+	 */
+	public function show($section_id, $id)
+	{
+		$section = Section::find($section_id);
+
+		// Send error if section does not exist
+		if (!$section) {
+			return $this->returnErrorMessage('section', 404, 'show group');
+		}
+
+		$group = $section->groups()->where('id', $id)->first();
+		if ($group) {
+			return $this->returnSuccessMessage('group', new GroupResource($group));
+		}
+
+		// Send error if group does not exist
+		return $this->returnErrorMessage('group', 404, 'show');
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int $section_id
+	 * @param  \Illuminate\Http\Request $request
+	 * @param  int $id
+	 * @return \Illuminate\Http\JsonResponse
+	 */
+	public function update($section_id, Request $request, $id)
+	{
+		$section = Section::find($section_id);
+
+		// Send error if section does not exist
+		if (!$section) {
+			return $this->returnErrorMessage('section', 404, 'update group');
+		}
+
+		$group = $section->groups()->where('id', $id)->first();
+
+		// Send error if group does not exist
+		if (!$group) {
+			return $this->returnErrorMessage('group', 404, 'update');
+		}
+
+		// Update group
+		if ($group->fill($request->only('name', 'repeatable'))->save()) {
+			return $this->returnSuccessMessage('group', new GroupResource($group));
+		}
+
+		// Send error if there is an error on update
+		return $this->returnErrorMessage('group', 503, 'update');
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int $section_id
+	 * @param  int $id
+	 * @return \Illuminate\Http\JsonResponse
+	 */
+	public function destroy($section_id, $id)
+	{
+		$section = Section::find($section_id);
+
+		// Send error if section does not exist
+		if (!$section) {
+			return $this->returnErrorMessage('section', 404, 'delete group');
+		}
+
+		$group = $section->groups()->where('id', $id)->first();
+
+		// Send error if group does not exist
+		if (!$group) {
+			return $this->returnErrorMessage('group', 404, 'delete');
+		}
+
+		if ($group->delete()) {
+			return $this->returnSuccessMessage('message', 'Group has been deleted successfully.');
+		}
+
+		// Send error if there is an error on update
+		return $this->returnErrorMessage('group', 503, 'delete');
+	}
 }
---
+++ app/Http/Resources/GroupResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class GroupResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'section_id' => $this->section_id,
+            'repeatable' => $this->repeatable
+        ];
+    }
+}
