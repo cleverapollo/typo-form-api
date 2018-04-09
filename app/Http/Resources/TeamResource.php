eb5332ac2c03646ac88ba6dca8e5317c82463533
cleverapollo <cleverapollo@gmail.com>
2018-04-09 14:32:28 +0200

    Add share_token to Team Resource

--- app/Http/Resources/TeamResource.php
+++ app/Http/Resources/TeamResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Role;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class TeamResource extends JsonResource
@@ -14,14 +15,17 @@ class TeamResource extends JsonResource
 	 */
 	public function toArray($request)
 	{
+		$team_role_id = $this->whenPivotLoaded('team_users', function () {
+			return $this->pivot->role_id;
+		});
+
 		return [
 			'id' => $this->id,
 			'name' => $this->name,
 			'description' => $this->description,
 			'application_id' => $this->application_id,
-			'team_role_id' => $this->whenPivotLoaded('team_users', function () {
-				return $this->pivot->role_id;
-			})
+			'team_role_id' => $team_role_id,
+			'share_token' => Role::find($team_role_id)->name == 'Admin' ? $this->share_token : null
 		];
 	}
 }
