5a9173b60362ef90bf3de5e3d80e76e36d9ce006
cleverapollo <cleverapollo@gmail.com>
2018-04-20 10:14:37 +0200

    Add css to applications table

---
+++ database/migrations/2018_04_20_080537_add_css_to_applications_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCssToApplicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            $table->text('css')->nullable()->after('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('applications', function (Blueprint $table) {
+            $table->dropColumn('css');
+        });
+    }
+}
