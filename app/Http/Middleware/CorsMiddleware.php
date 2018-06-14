58bdd6ec27c1b791dc7e1de656d1433ef6ed0db9
cleverapollo <cleverapollo@gmail.com>
2018-06-14 11:25:55 +0200

    Update CORS middleware

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -3,7 +3,6 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Response;
 
 class CorsMiddleware
 {
@@ -16,15 +15,22 @@ class CorsMiddleware
      */
     public function handle($request, Closure $next)
     {
+        $headers = [
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => 'HEAD, PATCH, POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Max-Age' => '86400',
+            'Access-Control-Allow-Headers' => $request->header('Access-Control-Request-Headers')
+        ];
+
         if ($request->isMethod('OPTIONS')) {
-            $response = new Response('', 200);
-        } else {
-            $response = $next($request);
+            return response()->json('{ "method": "OPTIONS" }', 200, $headers);
         }
 
-        $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, PATCH, DELETE');
-        $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
-        $response->header('Access-Control-Allow-Origin', '*');
+        $response = $next($request);
+        foreach ($headers as $key => $value) {
+            $response->header($key, $value);
+        }
 
         return $response;
     }
