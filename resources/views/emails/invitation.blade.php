77c81625a75d314970e693b5593a014ad930d26e
cleverapollo <cleverapollo@gmail.com>
2018-08-02 17:00:34 +1000

    Updated invitation email Updated session timeout to 24 hours.

--- resources/views/emails/invitation.blade.php
+++ resources/views/emails/invitation.blade.php
@@ -7,15 +7,7 @@
 
 <body>
     <h1>Join {{$name}} on Informed 365</h1>
-    <p>You have been invited to join the {{$type}} {{$name}} on Informed 365.</p>
-    <p>To accept this invitation:
-        <ol>
-            <li>Navigate to <a href="https://app.informed365.com">https://app.informed365.com</a></li>
-            <li>Log into your account. If you don't have an account you can crete a new account.</li>
-            <li>Click the invitation link below</li>
-        </ol>
-    </p>
-    <p>Please note you must be logged in to accept the invitation.</p>
-    <a href="{{config('mail.fronturl')}}/invitation/{{$type}}/{{$token}}">Accept invitation</a>
+    <p>You have been invited to join the {{$type}} {{$name}} on the Informed 365 platform.</p>
+    <p>To get started simply <a href="{{config('mail.fronturl')}}/invitation/{{$type}}/{{$token}}">click this link</a> and login or create an account.
 </body>
 </html>
