deb665fd4f536b942ddffc177fe171ffd6cab9eb
cleverapollo <cleverapollo@gmail.com>
2019-05-02 11:01:33 +1000

    Flatten migrations on wip branch. 	If using this branch, please rollback 8 migrations (php artisan migrate:rollback --step=8) then apply a fresh migrate

--- database/migrations/2019_04_29_081546_create_workflow_tables.php
+++ database/migrations/2019_04_29_081546_create_workflow_tables.php
@@ -17,29 +17,37 @@ class CreateWorkflowTables extends Migration
         Schema::create('workflows', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->json('config')->default('{}');
+            $table->unsignedInteger('application_id')->nullable();
             $table->unsignedInteger('author_id');
-            $table->foreign('author_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('trigger');
             $table->json('trigger_config')->default('{}');
             $table->string('action');
             $table->json('action_config')->default('{}');
-            $table->unsignedInteger('delay')->default(WorkflowRepository::WORKFLOW_STATUS_ACTIVE);
-            $table->integer('status');
+            $table->bigInteger('delay')->unsigned();
+            $table->integer('status')->default(WorkflowRepository::WORKFLOW_STATUS_ACTIVE);
             $table->datetime('active_from');
-            $table->datetime('active_to');
+            $table->datetime('active_to')->nullable();
+            $table->softDeletes();
             $table->timestamps();
+
+            $table->foreign('application_id')->references('id')->on('applications')->onDelete('cascade');
+            $table->foreign('author_id')->references('id')->on('users');
         });
 
         Schema::create('workflow_jobs', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('transaction_id');
             $table->integer('workflow_id');
-            $table->foreign('workflow_id')->references('id')->on('workflows')->onDelete('cascade');
             $table->datetime('scheduled_for');
             $table->json('data')->default('{}');
+            $table->string('signature')->default('');
             $table->datetime('completed_at')->nullable();
             $table->integer('status')->default(WorkflowRepository::JOB_STATUS_ACTIVE);
+            $table->string('message')->nullable();
             $table->timestamps();
+
+            $table->foreign('workflow_id')->references('id')->on('workflows')->onDelete('cascade');
         });
     }
 
